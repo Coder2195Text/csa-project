d407821c5a0dc815e0f5bdcaad8055ad2d1c79c3
Coder2195 <coder2195mail@gmail.com>
2023-09-14 20:36:54 -0400

    invenotory and constants

--- src/main/java/com/coder2195/game/Game.java
+++ src/main/java/com/coder2195/game/Game.java
@@ -2,6 +2,10 @@ package com.coder2195.game;
 
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import com.diogonunes.jcolor.Ansi;
+import com.diogonunes.jcolor.Attribute;
 
 enum GameState {
     MainScreen,
@@ -13,24 +17,44 @@ public class Game {
     public boolean isRunning = true;
     public GameState gameState = GameState.MainScreen;
 
-    char c;
-
-    public HashMap<Coordinates, Character> blockData = new HashMap();
+    public HashMap<Coordinates, Character> blockData = new HashMap<Coordinates, Character>();
 
     Game() {
 
     }
 
-    public void frame() {
-        Scanner sc = new Scanner(System.in);
-        if (gameState == GameState.MainScreen) {
+    void renderList(String[] list) {
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
 
+    private static void clear() {
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+        }
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public void frame() {
+        Game.clear();
+
+        if (gameState == GameState.MainScreen) {
+            renderList(Constants.MAIN_SCREEN_TEXT);
+            System.out.println(Ansi.colorize("Press enter to continue...", Attribute.RAPID_BLINK(),
+                    Attribute.YELLOW_TEXT(), Attribute.BOLD()));
+            Scanner sc = new Scanner(System.in);
+            sc.useDelimiter("");
+            sc.next();
+            sc.close();
+
+            gameState = GameState.Game;
+        } else {
+            System.out.println(Ansi.colorize("Press enter to continue...", Attribute.GREEN_TEXT(), Attribute.BOLD()));
         }
 
-        char c = sc.nextLine().charAt(0);
-        System.out.println(c);
-
-        
     }
-    
+
 }
--- src/main/java/com/coder2195/game/Inventory.java
+++ src/main/java/com/coder2195/game/Inventory.java
@@ -1,5 +1,53 @@
 package com.coder2195.game;
 
+import java.util.HashMap;
+
 public class Inventory {
-    
+  public static enum Items {
+    GRASS,
+    DIRT,
+    STONE,
+    WOOD,
+    LEAVES,
+    APPLES,
+    SWORD,
+    PICKAXE,
+    IRON,
+    DIAMONDS,
+    HELMET,
+    CHESTPLATE,
+    LEGGINGS,
+    BOOTS
+  }
+
+  public static HashMap<Items, String> itemData = new HashMap<Items, String>();
+
+  static {
+    itemData.put(Items.GRASS, "🌿");
+    itemData.put(Items.DIRT, "🟫");
+    itemData.put(Items.STONE, "🪨");
+    itemData.put(Items.WOOD, "🪵");
+    itemData.put(Items.LEAVES, "🍃");
+    itemData.put(Items.APPLES, "🍎");
+    itemData.put(Items.SWORD, "🗡️");
+    itemData.put(Items.PICKAXE, "⛏️");
+    itemData.put(Items.IRON, "🪙");
+    itemData.put(Items.DIAMONDS, "💎");
+    itemData.put(Items.HELMET, "🧢");
+    itemData.put(Items.CHESTPLATE, "🎽");
+    itemData.put(Items.LEGGINGS, "👖");
+    itemData.put(Items.BOOTS, "🥿");
+
+  }
+
+  public Inventory() {
+
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+
+    return "Inventory";
+  }
 }
